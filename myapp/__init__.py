1a5abfa5530abb97c03d81705521c480005b1f6e
goodv1ce <andriy.lysak.2003@gmail.com>
2023-12-28 18:53:41 +0200

    [FEAT] new feature - added jwt authorization

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,7 +10,12 @@ app.config.from_pyfile('config.py', silent=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pylab_user:xRcMuvQSZrjdMBiMczHYn66XSXc8IRpr@dpg-cm50k0un7f5s73c2e300-a.frankfurt-postgres.render.com/pylab_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
 db = SQLAlchemy(app)
+
+jwt = JWTManager(app)
+
 migrate = Migrate(app, db)
 
 from myapp.models import User, Category, Record
