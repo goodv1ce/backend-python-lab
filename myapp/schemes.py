d2252e6fffc94dfe15adeb9190d98fc1a8a7d5dc
goodv1ce <andriy.lysak.2003@gmail.com>
2023-12-28 19:25:31 +0200

    [UPDATE] update schemas

--- myapp/schemes.py
+++ myapp/schemes.py
@@ -17,7 +17,8 @@ class RecordSchema(Schema):
 class UserSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
-    default_currency_id = fields.Integer(required=True)
+    default_currency_name = fields.String()
+    password = fields.String(required=True)
 
 
 class CategorySchema(Schema):
